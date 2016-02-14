09d7e281b7e442bf6282d7b747fe3e6f8a77c1cd
Haseeb Qureshi <haseeb.n.qureshi@gmail.com>
2016-02-14 00:44:00 -0500

    Altered initial bootstrapping of package, requiring instantiation

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 
-var app = require('accelerated.api')();
-app.useMiddlewares([ 'angular' ]);
-app.useRoutes([ 'angular' ]);
-app.run();
+module.exports = function() {
+
+	var app = require('accelerated.api')();
+
+	app.useMiddlewares([ 'angular' ]);
+
+	app.useRoutes([ 'angular' ]);
+
+	app.run();
+
+	return app;
+
+};
+
