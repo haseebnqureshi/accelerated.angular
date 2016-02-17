e8a2583b4015043e3014ef648d4e3c4336f9d153
Haseeb Qureshi <haseeb.n.qureshi@gmail.com>
2016-02-17 09:59:55 -0500

    Contains small angular app, cli rsyncs to working dir

--- lib/angular/install.sh
+++ lib/angular/install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-cp $1/bower.json $2
-cp $1/.bowerrc $2
+# Copying necessary bower files into our specified angular directory
+rsync -avPI $1/www/ $2
+
+# Going to our angular dir, bower install, and then going back up
+cd $2
+bower install
+cd $1
